02b79fe971c76b05726a78807e0a9760513c736f
richard-ui <richard-jones96@hotmail.co.uk>
2021-04-05 10:41:59 +0000

    Made a start on developing products.html page using bootstrap Css features

--- products/models.py
+++ products/models.py
@@ -4,6 +4,10 @@ from django.db import models
 
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -25,5 +29,6 @@ class Product(models.Model):
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
-    def __str__(self):
-        return self.name
+
+def __str__(self):
+    return self.name
