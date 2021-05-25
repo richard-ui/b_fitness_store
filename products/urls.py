f3889d2efa310676bdd02535f58f0a8338d5e5b9
richard-ui <richard-jones96@hotmail.co.uk>
2021-05-25 18:38:58 +0000

    Add field for shoesizes inside the product model with dropdown for shoe sizes

--- products/urls.py
+++ products/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
         views.delete_product,
         name='delete_product'
         ),
-    path('auto', views.auto, name='auto'),
+    path('auto', views.auto, name='auto'),  # url for search box autocomplete
 ]
